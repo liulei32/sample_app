0002a44445218ed2dff4f44e1da6ced36c6c8c36
leiliu <liulei32@gmail.com>
2014-08-13 21:51:53 -0700

    Update title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,7 +8,7 @@ describe ApplicationHelper do
     end
 
     it "should include the base title" do
-      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+      expect(full_title("foo")).to match(/^京西网上商城/)
     end
 
     it "should not include a bar for the home page" do
